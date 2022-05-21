d3e473943ef064ac4ff404ed55e326911e180f18
Abinash Karmakar <darkrosesoftware@gmail.com>
2022-05-21 09:07:59 +0530

    PWA

--- js/index/index.js
+++ js/index/index.js
@@ -1,4 +1,4 @@
-import {menuOperation} from "./header.js"
+import { menuOperation } from "./header.js"
 import lib from "../lib.js"
 import load from "./load.js"
 
@@ -87,3 +87,35 @@ loadFromLink()
 
 
 lib()
+
+
+
+
+
+
+
+
+//Set service Worker
+
+const registerServiceWorker = async () => {
+    if ('serviceWorker' in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register(
+                '../sw.js'
+                // {
+                //     scope: '/sw-test/',
+                // }
+            );
+            // if (registration.installing) {
+            //     console.log('Service worker installing');
+            // } else if (registration.waiting) {
+            //     console.log('Service worker installed');
+            // } else if (registration.active) {
+            //     console.log('Service worker active');
+            // }
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
+    }
+};
+registerServiceWorker();
