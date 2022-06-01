d67f070a65f4848029da00f0462de1655683d082
Abinash Karmakar <codeAbinash@gmail.com>
2022-06-01 12:17:35 +0530

    Sidebar opening flash - fixed

--- js/index/header.js
+++ js/index/header.js
@@ -9,18 +9,33 @@ let menuOpen = true;
 export function menuOperation() {
     if (window.innerWidth >= 900)
         return
+
+    const animDuration = getAnimDuration()
     setTimeout(() => {
 
         if (menuOpen) {
             sideBar.style.left = "calc(-80%)"
-            sideBarCancel.style.left = "100%"
+            sideBarCancel.style.backgroundColor = "#00000000"
+            setTimeout(() => {
+                sideBarCancel.style.display = "none"
+            }, animDuration);
         } else {
             sideBar.style.left = 0
-            sideBarCancel.style.left = "0"
-
+            sideBarCancel.style.display = "block"
+            setTimeout(() => {
+                sideBarCancel.style.backgroundColor = "#00000033"
+            }, 10);
         }
     }, 50);
     menuOpen = !menuOpen
+
+
+    function getAnimDuration() {
+        const style = getComputedStyle(document.body)
+        const cssVar =  style.getPropertyValue('--anim-duration')
+        const animDuration = parseFloat(cssVar) * 1000
+        return animDuration
+    }
 }
 
 export default function handelMenu() {
