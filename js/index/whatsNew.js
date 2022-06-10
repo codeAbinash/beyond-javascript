762650aeb0bfff9de7e88f1ec95c66675d8a7a5a
Abinash Karmakar <codeAbinash@gmail.com>
2022-06-10 11:33:39 +0530

    Alert window button is now centered

--- js/index/whatsNew.js
+++ js/index/whatsNew.js
@@ -18,7 +18,17 @@ export default async function whatsNew(){
     }
 }
 
-
+new AlertWin({
+    heading: "Getting Started",
+    text: `<p><span class="bold">Beyond JavaScript</span> may contain spelling mistakes and syntactical or 
+        grammatical mistakes of \"English Language\". If you find any, please inform me at 
+        <a href="mailto:codeAbinash@gmail.com" class="emailPageError">codeAbinash@gmail.com</a> </br></br>
+        <span class="bold">Pro Tip :</span> Tap "Beyond JavaScript" (heading) to scroll to top of the page.
+        I did not use a button to do this, because it was looking terrible. 
+        </br></br><p class="bold center">"UI Matters!"</p>
+        </br></p>`,
+    btnTxt: "START READING"
+}).show()
 
 
  // Alert Window for first time
