e514fa574ddc71f62269e72dd313f649340d5e72
Abinash Karmakar <codeAbinash@gmail.com>
2022-06-20 21:56:21 +0530

    -w inequality operatro

---
+++ learning/js/operators/comparison/13.js
@@ -0,0 +1,5 @@
+let a = 5
+let b = 10
+
+a != b // true
+!(a == b) // true equivalent
---
+++ learning/js/operators/comparison/14.js
@@ -0,0 +1,17 @@
+1 != 5 // true
+1 != 1 // false
+"hello" != "hey" // true
+
+
+5 != "5" // false
+0 != null // true
+0 != undefined // true
+null!=undefined // false
+
+const string1 = "hello"
+const string2 = new String("hello")
+const string3 = String("hello")
+
+string1 != string2 // false
+string1 != string3 // false
+string2 != string3 // false
