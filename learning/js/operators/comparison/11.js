11a5230efe756263a6f31b4e79d61773a4cad7fd
Abinash Karmakar <codeAbinash@gmail.com>
2022-06-24 21:15:41 +0530

    fix -w

--- learning/js/operators/comparison/11.js
+++ learning/js/operators/comparison/11.js
@@ -5,13 +5,21 @@ null === null // true
 //Number type
 NaN === NaN // false
 100 === 100 // true
-+0 === -0 // true
++ 0 === -0 // true
+
+"1,2,3" === [1,2,3,] // false
+
 
 "Hello" === "Hello" // true
+"Hello" === new String("Hello") // false
+"Hello" === String("Hello") // true
+String("Hello") === new String("Hello") // false
+
+
 101 === 101 // true
 
-let obj1 = {name : "Pekachu"}
-let obj2 = {name : "Pekachu"}
+let obj1 = { name: "Pekachu" }
+let obj2 = { name: "Pekachu" }
 
 obj1 === obj2  // false, different object
 obj1 === obj1  // true, same object
---
+++ learning/js/operators/comparison/lt/14.js
@@ -0,0 +1,7 @@
+let a = new Date("2021")
+let b = new Date("2020")
+
+console.log(Number(a))
+console.log(Number(b))
+
+console.log(a < b) // false
