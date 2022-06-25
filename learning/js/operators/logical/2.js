c11b176b02bc4c18972a11e2e7d2f7f30ef8d429
Abinash Karmakar <codeAbinash@gmail.com>
2022-06-25 21:36:55 +0530

    Fix -w

--- learning/js/operators/logical/2.js
+++ learning/js/operators/logical/2.js
@@ -1,14 +1,19 @@
 // All these values are true
 
 true
+
 1
 5413
+10n
+
 "Hello"
 " " // whitespace
+"false"
+
 {} // empty object
 [] // empty array
+
 new Number()
+
 Infinity
 -Infinity
-"false"
-10n
