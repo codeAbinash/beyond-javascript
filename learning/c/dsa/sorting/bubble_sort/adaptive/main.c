c967b4523ed0bc07adf6ded6085ecf1e32e97169
Abinash Karmakar <codeAbinash@gmail.com>
2022-07-13 21:13:37 +0530

    Bubble Sort Adaptive

---
+++ learning/c/dsa/sorting/bubble_sort/adaptive/main.c
@@ -0,0 +1,32 @@
+#include "stdio.h"
+
+void dispArr(int *arr, int len) {
+    int i;
+    for (i = 0; i < len; i++) printf("%d ", arr[i]);
+    printf("\n");
+}
+
+void bubbleSortAdaptive(int *arr, int len) {
+    int i, j, tmp, isSorted;
+    for (i = 0; i < len - 1; i++) {
+        isSorted = 1;
+        for (j = 0; j < len - i - 1; j++)
+            if (arr[j] > arr[j + 1]) {
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+                isSorted = 0;
+            }
+        if (isSorted) return;
+    }
+}
+
+int main() {
+    int arr[] = {9, 458, 451, 874, 45, 255, 1, 54, 45, 7, 1};
+    int len = sizeof(arr) / sizeof(int);
+
+    dispArr(arr, len);
+    bubbleSortAdaptive(arr, len);
+    dispArr(arr, len);
+    return 0;
+}
