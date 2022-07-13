d8a3c68cd1ca84254a5c655f7e29188ef6fe5c06
Abinash Karmakar <codeAbinash@gmail.com>
2022-07-13 20:56:03 +0530

    Added Bubble Sort

---
+++ learning/c/dsa/sorting/bubble_sort/main.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+
+void dispArr(int *arr,int len){
+    int i;
+    for(i=0;i<len;i++)
+        printf("%d ",arr[i]);
+    printf("\n");
+
+}
+
+void bubbleSort(int *arr, int len){ 
+    int i,j,tmp;
+    for(i=0;i<len-1;i++)
+        for(j=0;j<len-i-1;j++)
+            if(arr[j]>arr[j+1]){
+                tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+}
+
+int main(){
+    int arr[] = {9,458,451,874,45,255,1,54,45,7,1};
+    int len = sizeof(arr) / sizeof(int);
+    
+    dispArr(arr,len);
+    bubbleSort(arr,len);
+    dispArr(arr,len);
+    return 0;
+}
+
