d2e609df3f59285084f6ad21c9ac825220cc0998
Abinash Karmakar <codeAbinash@gmail.com>
2022-07-14 20:22:00 +0530

    Quicksort

---
+++ learning/js/dsa/sorting/quick_sort/main.js
@@ -0,0 +1,29 @@
+function quick_Sort(origArray) {
+    if (origArray.length <= 1) {
+        return origArray;
+    } else {
+
+        var left = [];
+        var right = [];
+        var newArray = [];
+        var pivot = origArray.pop();
+        var length = origArray.length;
+
+        for (var i = 0; i < length; i++) {
+            if (origArray[i] <= pivot) {
+                left.push(origArray[i]);
+            } else {
+                right.push(origArray[i]);
+            }
+        }
+
+        return newArray.concat(quick_Sort(left), pivot, quick_Sort(right));
+    }
+}
+
+let arr = [4, 8, 98, 1, 2, 6, 9, 58, 5, 5, 6, 58]
+
+console.log(arr)
+arr = quick_Sort(arr)
+console.log("After Sorting...")
+console.log(arr)
