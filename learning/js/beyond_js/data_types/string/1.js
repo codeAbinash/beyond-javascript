1f8aa09703e127d555e1eed404af82b47fdf7db1
Abinash Karmakar <codeAbinash@gmail.com>
2022-08-09 23:22:38 +0530

    string

---
+++ learning/js/beyond_js/data_types/string/1.js
@@ -0,0 +1,7 @@
+let myName = 'Abinash' // using single quote
+let friend1 = "Tukai" // using double quote
+let friend2 = `Rohan` // using backtick
+
+console.log(myName)
+console.log(friend1)
+console.log(friend2)
