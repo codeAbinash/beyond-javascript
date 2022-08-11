3d4124732a5ac2265eb07dd73b34d21a9555792e
Abinash Karmakar <codeAbinash@gmail.com>
2022-08-11 12:00:13 +0530

    string methods

---
+++ learning/js/beyond_js/data_types/string/27.js
@@ -0,0 +1,4 @@
+"a".toUpperCase() // 'A'
+"A".toUpperCase() // 'A'
+"JavaScript".toUpperCase() // 'JAVASCRIPT'
+"123-*/@#$%^".toUpperCase() // '123-*/@#$%^'
---
+++ learning/js/beyond_js/data_types/string/28.js
@@ -0,0 +1,4 @@
+"a".toLowerCase() // 'a'
+"A".toLowerCase() // 'a'
+"JavaScript".toLowerCase() // 'javascript'
+"123-*/@#$%^".toLowerCase() // '123-*/@#$%^'
